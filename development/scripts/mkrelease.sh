45b3a42a9e5405788bd7da4fad011dd05157928b
moberhuber <moberhuber>
2007-05-29 12:07:43 +0000

    Add mkrelease.sh

--- development/scripts/mkrelease.sh
+++ development/scripts/mkrelease.sh
@@ -3,12 +3,14 @@ curdir=`pwd`
 cd `dirname $0`
 
 RELEASE=2.0RC1
-mkdir line_count_scripts
-cp lc lc.bat lcp lcp.bat line_count_scripts
-cp notice.html line_count_scripts
-chmod 755 line_count_scripts/lc*
-chmod 644 line_count_scripts/lc*.bat
-chmod 644 line_count_scripts/notice.html
-tar cfv line_count_scritps-${RELEASE}.tar line_count_scripts
-zip line_count_scripts-${RELEASE}.zip line_count_scripts
-rm -rf line_count_scripts
+ARCHIVE_NAME=${ARCHIVE_NAME}
+
+mkdir ${ARCHIVE_NAME}
+cp lc lc.bat lcp lcp.bat ${ARCHIVE_NAME}
+cp notice.html ${ARCHIVE_NAME}
+chmod 755 ${ARCHIVE_NAME}/lc*
+chmod 644 ${ARCHIVE_NAME}/lc*.bat
+chmod 644 ${ARCHIVE_NAME}/notice.html
+tar cfv ${ARCHIVE_NAME}-${RELEASE}.tar ${ARCHIVE_NAME}
+zip ${ARCHIVE_NAME}-${RELEASE}.zip ${ARCHIVE_NAME}
+rm -rf ${ARCHIVE_NAME}
