2365d12f7f013446c500a4a4d4b059e2ad243663
moberhuber <moberhuber>
2007-05-29 11:00:45 +0000

    Add -e parameter to lc, lcp shellscripts for including empty lines in count and improve *.bat -> cygwin runner

--- development/scripts/lc.bat
+++ development/scripts/lc.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM ******************************************************************************
-REM # Copyright (c) 2006 Wind River Systems, Inc.
+REM # Copyright (c) 2006, 2007 Wind River Systems, Inc.
 REM # All rights reserved. This program and the accompanying materials 
 REM # are made available under the terms of the Eclipse Public License v1.0 
 REM # which accompanies this distribution, and is available at 
@@ -15,11 +15,46 @@ REM #:# data and documentation of all files below a list of directories.
 REM #:# See the "lc" script for details.
 REM #:#
 REM #:# Usage:
-REM #:#    lc [-fh] {directory,...}
+REM #:#    lc [-efh] {directory,...}
+REM #:# Options:
+REM #:#    -e  ..  include empty lines in count (like wc -l)
+REM #:#    -f  ..  show file list on stderr (for debugging)
+REM #:#    -h  ..  show help
+REM #:# Examples:
+REM #:#    lc -f com.foobar.plugin
+REM #:#    lc -e org.eclipse.core.*
 REM #:#
-IF "%CYGWIN_BIN%" == "" SET CYGWIN_BIN=E:\Apps\Cygwin\bin
-IF "%ECL_SCRIPTS%" == "" SET ECL_SCRIPTS=F:\WS\rse.latest\www-tm-development\scripts
+SETLOCAL
+SET MYDIR=%~dp0
 SHIFT 1
-SET PATH=%CYGWIN_BIN%;%PATH%
-%CYGWIN_BIN%\bash.exe -x %ECL_SCRIPTS%\lc %*
 
+IF "%ECL_SCRIPTS%" == "" SET ECL_SCRIPTS=%MYDIR%
+IF NOT EXIST %ECL_SCRIPTS%\lcp SET ECL_SCRIPTS=.
+IF NOT EXIST %ECL_SCRIPTS%\lcp SET ECL_SCRIPTS=C:\usr\bin\scripts
+IF NOT EXIST %ECL_SCRIPTS%\lcp GOTO err_no_scripts
+
+IF "%CYGWIN_BIN%" == "" SET CYGWIN_BIN=E:\Apps\Cygwin\bin
+IF NOT EXIST %CYGWIN_BIN%\bash.exe SET CYGWIN_BIN=C:\Cygwin\bin
+IF NOT EXIST %CYGWIN_BIN%\bash.exe GOTO cygwin_in_path
+SET PATH=%CYGWIN_BIN%;%PATH%
+REM Use bash.exe -x for debugging
+REM %CYGWIN_BIN%\bash.exe -x %ECL_SCRIPTS%\lc %*
+%CYGWIN_BIN%\bash.exe %ECL_SCRIPTS%\lc %*
+GOTO end
+:cygwin_in_path
+bash -c echo 2> NUL:
+IF ERRORLEVEL 1 GOTO err_no_cygwin
+bash %ECL_SCRIPTS%\lc %*
+GOTO end
+
+:err_no_cygwin
+ECHO.
+ECHO Error: Cygwin bash.exe not found.
+ECHO Please edit lc.bat to contain the directory of your cygwin installation.
+GOTO end
+:err_no_scripts
+ECHO.
+ECHO Error: lc shellscript not found.
+ECHO Please edit lc.bat to contain the directory of your downloaded lc script.
+:end
+ENDLOCAL
--- development/scripts/lcp.bat
+++ development/scripts/lcp.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM ******************************************************************************
-REM # Copyright (c) 2006 Wind River Systems, Inc.
+REM # Copyright (c) 2006, 2007 Wind River Systems, Inc.
 REM # All rights reserved. This program and the accompanying materials 
 REM # are made available under the terms of the Eclipse Public License v1.0 
 REM # which accompanies this distribution, and is available at 
@@ -15,10 +15,44 @@ REM #:# data and documentation in a file submitted as a patch.
 REM #:# See the "lcp" script for details.
 REM #:#
 REM #:# Usage:
-REM #:#    lcp {patchfile}
+REM #:#    lcp [-eh] {patchfile}
+REM #:# Options:
+REM #:#    -e  ..  include empty lines in count (like wc -l)
+REM #:#    -h  ..  show help
+REM #:# Examples:
+REM #:#    lcp bug12345.patch.diff
 REM #:#
-IF "%CYGWIN_BIN%" == "" SET CYGWIN_BIN=E:\Apps\Cygwin\bin
-IF "%ECL_SCRIPTS%" == "" SET ECL_SCRIPTS=F:\WS\rse.latest\www-tm-development\scripts
+SETLOCAL
+SET MYDIR=%~dp0
 SHIFT 1
+
+IF "%ECL_SCRIPTS%" == "" SET ECL_SCRIPTS=%MYDIR%
+IF NOT EXIST %ECL_SCRIPTS%\lcp SET ECL_SCRIPTS=.
+IF NOT EXIST %ECL_SCRIPTS%\lcp SET ECL_SCRIPTS=C:\usr\bin\scripts
+IF NOT EXIST %ECL_SCRIPTS%\lcp GOTO err_no_scripts
+
+IF "%CYGWIN_BIN%" == "" SET CYGWIN_BIN=E:\Apps\Cygwin\bin
+IF NOT EXIST %CYGWIN_BIN%\bash.exe SET CYGWIN_BIN=C:\Cygwin\bin
+IF NOT EXIST %CYGWIN_BIN%\bash.exe GOTO cygwin_in_path
 SET PATH=%CYGWIN_BIN%;%PATH%
-%CYGWIN_BIN%\bash.exe -x %ECL_SCRIPTS%\lcp %*
+REM Use bash.exe -x for debugging
+REM %CYGWIN_BIN%\bash.exe -x %ECL_SCRIPTS%\lcp %*
+%CYGWIN_BIN%\bash.exe %ECL_SCRIPTS%\lcp %*
+GOTO end
+:cygwin_in_path
+bash -c echo 2> NUL:
+IF ERRORLEVEL 1 GOTO err_no_cygwin
+bash %ECL_SCRIPTS%\lcp %*
+GOTO end
+
+:err_no_cygwin
+ECHO.
+ECHO Error: Cygwin bash.exe not found.
+ECHO Please edit lcp.bat to contain the directory of your cygwin installation.
+GOTO end
+:err_no_scripts
+ECHO.
+ECHO Error: lcp shellscript not found.
+ECHO Please edit lcp.bat to contain the directory of your downloaded lcp script.
+:end
+ENDLOCAL
