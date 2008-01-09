7e7c9298bad471f353f61ebbe4f1ca607b011e2a
moberhuber <moberhuber>
2008-01-09 16:51:30 +0000

    Link to current API docs from the docs page

--- doc/index.php
+++ doc/index.php
@@ -40,6 +40,15 @@
     	<li><a href="/dsdp/tm/doc/nucleus_edge_target_usecase.pdf">nucleus edge target usecase.pdf</a></li>
     	</ul>
 
+		<h2>Current API Docs</h2>
+		These are part of the downloaded SDK, and available online. The online docs are automatically generated
+		from the latest code every night.
+		<ul>
+		  <li><a href="http://dsdp.eclipse.org/help/latest/topic/org.eclipse.rse.doc.user/gettingstarted/g_start.html">RSE User Guide</a></li>
+		  <li><a href="http://dsdp.eclipse.org/help/latest/topic/org.eclipse.rse.doc.isv/guide/rse_int.html">RSE Developer Guide</a></li>
+		  <li><a href="http://dsdp.eclipse.org/help/latest/topic/org.eclipse.dstore.doc.isv/toc.html">DStore Developer Guide</a></li>
+		</ul>
+
 		<h2>Design Documents</h2>
 		<ul>
 	    <li><a href="/dsdp/tm/meetingnotes/ff01_chicago/DSDPTM_Overview.ppt">DSDP-TM Overview (ppt)</a></li>
