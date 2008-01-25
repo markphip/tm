e62873c9b3dc9838f6be785005a5c4db0988ab34
kdoyle <kdoyle>
2008-01-25 20:01:36 +0000

    Fixed News.

--- tm-new/index.php
+++ tm-new/index.php
@@ -35,7 +35,7 @@
 	<div id="midcolumn" style="width:93%">
 		<table border="0" cellpadding="5">
 		<tr>
-			<td><a href=""><img alt="Remote System Explorer Perspective" src=""></img></td>
+			<td><a href=""><img alt="Remote System Explorer Perspective" src=""></img></a></td>
 			<td>
 				<p>The Target Management project creates data models and frameworks to configure and manage remote systems, their connections, and their services.  Our main offering is the Remote System Explorer (RSE), which integrates any sort of heterogeneous remote resources under a single, consistent UI and allows transparent working on remote computers just like the local one. Other offerings include a lightweight Terminal and a Network Discovery framework. <a href="">More Information</a></p>
 				<h2><a href="/dsdp/tm/tm-new/tutorial">Getting Started</a></h2>
@@ -65,6 +65,18 @@
 				         <a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tm/presentations/2007-10-10_TM_ESE2007.ppt">PPT</a> |
 				         <a href="http://www.eclipse.org/downloads/download.php?file=/dsdp/tm/presentations/2007-10-10_TM_ESE2007.pdf">PDF</a>)
 					</li></ul>
+				</li>
+				<li>
+					Sep 28th: <a href="http://tmober.blogspot.com/2007/09/tm-201-can-terminal-be-too-fast.html">TM 2.0.1</a> Service Release
+				</li>
+				<li>
+					September 17-19, 2007:  <a href="http://wiki.eclipse.org/DSDP/TM/Face-to-face_Meeting_Toronto_17-Sep-2007">TM Planning Meeting and Coding Camp</a>, Toronto
+				</li>
+				<li>
+					Jun 29th: <a href="http://download.eclipse.org/dsdp/tm/downloads/drops/R-2.0-200706270925/">TM 2.0</a> has been released!	
+				</li>
+				<li>
+					April 12, 2007: <a href="http://live.eclipse.org/node/229">Webinar</a>: TM goals, architecture, future plans and online demo (<a href="http://live.eclipse.org/node/229">50 minute full recording</a> | <a href="http://www.eclipse.org/projects/slides/TM_Webinar_Slides_070412.ppt">PPT slides</a>)
 				</li>
 			</ul>
 		</div>
