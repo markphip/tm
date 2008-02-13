fb8937165739b46a38542367ef1fa76c63ab3025
moberhuber <moberhuber>
2008-02-13 10:59:07 +0000

    Fix default basebuilder branch

--- rse/build/_common.php
+++ rse/build/_common.php
@@ -2,7 +2,7 @@
 require_once ("../../../includes/buildServer-common.php");
 
 $options = array (
-	"BaseBuilderBranch" => "v20071108",
+	"BaseBuilderBranch" => "M4_34",
 	
 	"BranchAndJDK" => array (
 		"dsdp.eclipse.org=------------,------------",
