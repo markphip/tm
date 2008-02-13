9fde8c9bd6ede82fa19c1885db3092a3c62aa281
moberhuber <moberhuber>
2008-02-13 13:44:35 +0000

    Fix TM Sideitems

--- build/sideitems-common.php
+++ build/sideitems-common.php
@@ -19,10 +19,10 @@ function sidebar()
 	<div class="sideitem">
 		<h6>Info</h6>
 		<ul>
-			<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=EMFT&amp;component=<?php echo $projct; ?>&amp;bug_status=ASSIGNED">Assigned Bugs</a></li>
-			<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=EMFT&amp;component=<?php echo $projct; ?>&amp;bug_status=RESOLVED">Resolved Bugs</a></li>
-			<li><a href="http://www.eclipse.org/modeling/mdt/searchcvs.php?q=branch%3A+HEAD+days%3A+7">Development This Week</a></li>
-			<li><a href="http://www.eclipse.org/modeling/mdt/searchcvs.php?q=branch%3A+R+days%3A+7">Maintenance This Week</a></li>
+			<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=Target+Management&amp;component=<?php echo $projct; ?>&amp;bug_status=ASSIGNED">Assigned Bugs</a></li>
+			<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?product=Target+Management&amp;component=<?php echo $projct; ?>&amp;bug_status=RESOLVED">Resolved Bugs</a></li>
+			<li><a href="http://www.eclipse.org/dsdp/tm/searchcvs.php?q=branch%3A+HEAD+days%3A+7">Development This Week</a></li>
+			<li><a href="http://www.eclipse.org/dsdp/tm/searchcvs.php?q=branch%3A+R+days%3A+7">Maintenance This Week</a></li>
 			<!-- <li><a href="http://emf.torolab.ibm.com/<?php print $PR; ?>/downloads/downloads.php">Download Stats</a></li> -->
 			<li><a href="http://wiki.eclipse.org/Modeling_Project_Releng">Modeling Project Releng Help</a></li>
 		</ul>
