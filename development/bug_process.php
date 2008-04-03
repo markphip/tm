032611b0cd996db35e80ae6da77c81458cae587e
moberhuber <moberhuber>
2008-04-03 16:23:21 +0000

    Add TCF and Terminal queries to bug_process page

--- development/bug_process.php
+++ development/bug_process.php
@@ -52,7 +52,11 @@
 		  <li>TM and RSE <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&classification=DSDP&product=Target+Management&target_milestone=2.0.3&cmdtype=doit">
 		    2.0.3 assigned bugs</a></li>
 		  <li>TM and RSE <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&classification=DSDP&product=Target+Management&target_milestone=3.0+M6&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&cmdtype=doit">
-		    3.0M6 assigned bugs</a></li>  
+		    3.0M6 assigned bugs</a></li>
+		  <li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&classification=DSDP&product=Target+Management&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&cmdtype=doit&field0-0-0=component&type0-0-0=equals&value0-0-0=TCF&field0-0-1=assigned_to&type0-0-1=equals&value0-0-1=dsdp.tm.tcf-inbox%40eclipse.org&field0-0-2=short_desc&type0-0-2=anywords&value0-0-2=tcf">
+		    TCF open bugs</a></li>
+		  <li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&classification=DSDP&product=Target+Management&bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&cmdtype=doit&field0-0-0=component&type0-0-0=equals&value0-0-0=Terminal&field0-0-1=assigned_to&type0-0-1=equals&value0-0-1=dsdp.tm.core-inbox%40eclipse.org&field0-0-2=short_desc&type0-0-2=anywords&value0-0-2=terminal">
+		    Terminal open bugs</a></li>
 		  <li>TM and RSE <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&classification=DSDP&product=Target+Management&target_milestone=Future&cmdtype=doit">
 		    bugs deferred to the future</a></li>
 		</ul>
