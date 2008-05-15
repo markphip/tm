ab8ca0a409f4e36f004d5533a47f7c3652efe6c6
moberhuber <moberhuber>
2008-05-15 20:07:57 +0000

    This commit was manufactured by cvs2svn to create tag 'TM_3_0_IP_REVIEW_SUBMISSION'.

--- build/removeRelease.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once ("../../includes/buildServer-common.php");
-require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/removeRelease-common.php");
-?>
--- feeds/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once ("../../includes/buildServer-common.php");
-require($_SERVER["DOCUMENT_ROOT"] . "/dsdp/includes/feeds-common.php");
-?>
