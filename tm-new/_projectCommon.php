f9603ef5ff48fddf3e250fb5058096980f7a20dd
ddykstal <ddykstal>
2009-01-23 23:24:58 +0000

    [238574][website] Revamp the TM main website - fixing menu

--- tm-new/_projectCommon.php
+++ tm-new/_projectCommon.php
@@ -24,7 +24,7 @@ EOBRANDING;
 
 	$Menu->setProjectBranding($branding);
 	
-	$Nav->addNavSeparator("Target Management", 	"/dsdp/tm/tm-new/", "_self", 1);
+	$Nav->addNavSeparator("Target Management");
 	$Nav->addCustomNav("About", "/dsdp/tm/tm-new/about", "_self", 1);
 	$Nav->addCustomNav("Team", "/dsdp/tm/tm-new/team",	"_self", 1);
 	$Nav->addCustomNav("Plan", "/dsdp/tm/tm-new/plan", "_self", 1);
@@ -34,14 +34,14 @@ EOBRANDING;
 	$Nav->addCustomNav("Summary", "http://www.eclipse.org/projects/project_summary.php?projectid=dsdp.tm", "_self", 1);
 	$Nav->addCustomNav("Legal", "dsdp/tm/development/tm-log.csv", "_self", 1);
 
-	$Nav->addNavSeparator("Users", "/dsdp/tm/tm-new/tutorial", "_self", 1);
+	$Nav->addNavSeparator("Users");
 	$Nav->addCustomNav("Getting Started", "/dsdp/tm/tm-new/tutorial", "_self", 1);
 	$Nav->addCustomNav("Downloads", "/dsdp/tm/tm-new/downloads", "_self", 1);
 	$Nav->addCustomNav("Bugs", "/dsdp/tm/tm-new/bugs", "_self", 1);
 	$Nav->addCustomNav("FAQ (wiki)", "http://wiki.eclipse.org/TM_and_RSE_FAQ", "_self", 1);
 	$Nav->addCustomNav("Newsgroup", "http://dev.eclipse.org/newslists/news.eclipse.dsdp.tm/maillist.html", "_self", 1);
 
-	$Nav->addNavSeparator("Contributors (wiki)", "http://wiki.eclipse.org/TM_and_RSE_FAQ#Working_on_TM_.2F_RSE", "_self", 1);
+	$Nav->addNavSeparator("Contributors (wiki)");
 	$Nav->addCustomNav("Contributing (wiki)", "http://wiki.eclipse.org/TM_and_RSE_FAQ#Working_on_TM_.2F_RSE", "_self", 1);
 	$Nav->addCustomNav("Mailing List", "https://dev.eclipse.org/mailman/listinfo/dsdp-tm-dev", "_self", 1);
 	$Nav->addCustomNav("CVS", "/dsdp/tm/tm-new/cvs/", "_self", 1);
