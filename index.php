e58b00427985a1d3bd2319b99bd0e393cc6cac20
moberhuber <moberhuber>
2011-03-15 02:06:21 +0000

    Release TM 3.3m6

--- index.php
+++ index.php
@@ -77,15 +77,14 @@
 		for more information.</p>
 
 		<p><font size="+0"><b>Latest Milestone:</b> 
-		<a href="http://download.eclipse.org/tm/downloads/drops/S-3.3M5-201102011100/">
-		TM 3.3M5</a></font>
-		[<a href="http://download.eclipse.org/tm/downloads/drops/S-3.3M5-201102011100/">downloads</a> 
+		<a href="http://download.eclipse.org/tm/downloads/drops/S-3.3M6-201103142031/">
+		TM 3.3M6</a></font>
+		[<a href="http://download.eclipse.org/tm/downloads/drops/S-3.3M6-201103142031/">downloads</a> 
 		| <a href="http://download.eclipse.org/tm/updates/3.3milestones/">update site</a>
-		| <a href="http://download.eclipse.org/tm/downloads/drops/S-3.3M5-201102011100/buildNotes.php">
+		| <a href="http://download.eclipse.org/tm/downloads/drops/S-3.3M6-201103142031/buildNotes.php">
 		build notes</a>]<br/>
-		TM 3.3M5 contains a new Commons Net version for IPv6 support, an SSH subsystem performance 
-		improvement, and several fixes to the TM Terminal widget. See the 
-		<a href="http://download.eclipse.org/tm/downloads/drops/S-3.3M5-201102011100/buildNotes.php">
+		TM 3.3M6 contains several bugfixes. See the 
+		<a href="http://download.eclipse.org/tm/downloads/drops/S-3.3M6-201103142031/buildNotes.php">
 		build notes</a> for new&amp;noteworthy and more information.
 		<!-- TODO Enable for ramp-down
 		<br/>
