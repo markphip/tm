f7d24862afdd06e9595d1a34cccafa07be627962
moberhuber <moberhuber>
2011-10-12 15:32:47 +0000

    Bug 360651 - The TM homepage is empty

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,22 +1,4 @@
 <?php
-
-# <modeling variables>
-$PR = "tm";
-$projectName = "Target Management";
-
-$isEMFserver = (preg_match("/^emf(?:\.torolab\.ibm\.com)$/", $_SERVER["SERVER_NAME"]));
-$isBuildServer = (preg_match("/^(dsdp|build)\.eclipse\.org$/", $_SERVER["SERVER_NAME"])) || $isEMFserver;
-$isBuildDotEclipseServer = $_SERVER["SERVER_NAME"] == "build.eclipse.org";
-$isWWWserver = (preg_match("/^(?:www.|)eclipse.org$/", $_SERVER["SERVER_NAME"]));
-$isEclipseCluster = (preg_match("/^(?:www.||download.|download1.|build.)eclipse.org$/", $_SERVER["SERVER_NAME"]));
-$debug = (isset ($_GET["debug"]) && preg_match("/^\d+$/", $_GET["debug"]) ? $_GET["debug"] : -1);
-$writableRoot = ($isBuildServer ? $_SERVER["DOCUMENT_ROOT"] . "/dsdp/includes/" : "/home/data/httpd/writable/dsdp/");
-$writableBuildRoot = $isBuildDotEclipseServer ? "/opt/public/dsdp" : "/home/www-data";
-
-$rooturl = "http://" . $_SERVER["HTTP_HOST"] . "/$PR";
-$downurl = ($isBuildServer ? "" : "http://www.eclipse.org");
-$bugurl = "https://bugs.eclipse.org";
-
 # Set the theme for your project's web pages.
 # See the Committer Tools "How Do I" for list of themes
 # https://dev.eclipse.org/committers/
@@ -31,60 +13,6 @@ else
 	$theme = "Lazarus";
 }
 
-/* projects/components in cvs */
-/* "proj" => "cvsname" */
-#$cvsprojs = array (); /* should always be empty */
-$cvsprojs = array (
-	"rse" => "org.eclipse.tm.rse"
-);
-
-/* sub-projects/components in cvs for projects/components above (if any) */
-/* "cvsname" => array("shortname" => "cvs-subname") */
-$cvscoms = array(
-	"org.eclipse.tm.core" => array (
-		"terminal" => "terminal",
-		"discovery" => "discovery"
-		/* add more here */
-	)
-);
-
-/* Readable Project names: "bugzilla component" => "shortname" */
-$projects = array(
-	"RSE" => "rse",
-	#"TCF" => "tcf",
-	"Terminal" => "terminal",
-	#"Core" => "discovery",
-);
-
-$bugcoms = array_flip($projects);
-$bugcoms = preg_replace("/ /", "%20", $bugcoms);
-
-$extraprojects = array(); //components with only downloads, no info yet, "prettyname" => "directory"
-$nodownloads = array("coordinated","jcrm","temporality", "emf4net"); //components with only information, no downloads, or no builds available yet, "projectkey"
-$nonewsgroup = array(); //components without newsgroup
-$nomailinglist = array(); //components without mailinglist
-#$incubating = $projects; // ALL components are incubating
-$incubating = array("tcf"); //Only TCF is incubating for now
-$hasmoved = array(
-	"query" => "emf", "transaction" => "emf", "validation" => "emf",
-	"eodm" => "mdt", "ocl" => "mdt",
-	"jet" => "m2t", "jeteditor" => "m2t"); // components which have moved, and to where
-$nomenclature = "Component"; //are we dealing with "components" or "projects"?
-
-#360651#include_once $_SERVER["DOCUMENT_ROOT"] . "/dsdp/includes/scripts.php";
-
-$regs = null;
-$proj = (isset($_GET["project"]) && preg_match("/^(" . join("|", $projects) . ")$/", $_GET["project"], $regs) ? $regs[1] : getProjectFromPath($PR));
-$projct= preg_replace("#^/#", "", $proj);
-
-$buildtypes = array(
-	"R" => "Release",
-	"S" => "Stable",
-	"I" => "Integration",
-	"M" => "Maintenance",
-	"N" => "Nightly"
-);
-# </modeling variables>
 
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
@@ -133,8 +61,5 @@ EOBRANDING;
 	$Nav->addCustomNav("Search CVS", "/$PR/searchcvs.php?q=file%3A+org.eclipse.tm%25%2F" . "+days%3A+7", "_self", 2);
 
 	$collist = "%26query_format%3Dadvanced&amp;column_changeddate=on&amp;column_bug_severity=on&amp;column_priority=on&amp;column_rep_platform=on&amp;column_bug_status=on&amp;column_product=on&amp;column_component=on&amp;column_version=on&amp;column_target_milestone=on&amp;column_short_short_desc=on&amp;splitheader=0";
-	#$Nav->addCustomNav("Open Bugs", "$bugurl/bugs/colchange.cgi?rememberedquery=product%3D" . $projectName . (isset ($bugcoms[$proj]) ? "%26component=$bugcoms[$proj]" : "") . "%26bug_status%3DNEW%26bug_status%3DASSIGNED%26bug_status%3DREOPENED%26order%3Dbugs.bug_status%2Cbugs.target_milestone%2Cbugs.bug_id" . $collist, "_self", 2);
-
-#$App->AddExtraHtmlHeader("<link rel=\"stylesheet\" type=\"text/css\" href=\"/dsdp/includes/common.css\"/>\n");
 unset ($bugcoms);
 ?>
